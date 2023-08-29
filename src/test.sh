4a72681185c83d917e2839c028267cf37bbadcd9
scherbinek <scherbinek@users.noreply.github.com>
2023-08-29 18:51:53 +0200

    Delete test.sh

--- src/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-    ## Set Environment Variables
-export $(grep -v '^#' ./resources/.env | xargs)
-echo $MESSAGE
-    
-/usr/bin/rsync -azrd --delete -e 'ssh -p23 -o StrictHostKeyChecking=no' $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_DOCKER/ ./resources/dockerData
