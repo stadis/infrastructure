6c4b893951c2a10e536c9fe03c7f17fbaed86705
scherbinek <scherbinek@users.noreply.github.com>
2023-08-29 20:52:31 +0200

    Delete test.sh

--- src/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export $(grep -v '^#' ./resources/.env | xargs)
-echo $MESSAGE
-
-/usr/bin/rsync --progress -e '/usr/bin/sshpass -p $RSYNC_PASSWORD ssh -p23' -azrd --delete /etc/letsencrypt/* $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_WWW_LETSENCRYPT/
