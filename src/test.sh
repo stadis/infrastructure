b40c7b8deb8bfeee5bce400e7413ec1faec16b3b
scherbinek <scherbinek@users.noreply.github.com>
2023-08-29 19:45:28 +0200

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -3,4 +3,4 @@
 export $(grep -v '^#' ./resources/.env | xargs)
 echo $MESSAGE
 
-/usr/bin/rsync -e 'ssh -p23' -azrd --delete /etc/letsencrypt/* $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_WWW_LETSENCRYPT/
+/usr/bin/rsync -e 'ssh -p23 $RSYNC__DESTSSHINFO' -azrd --delete /etc/letsencrypt/* $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_WWW_LETSENCRYPT/
