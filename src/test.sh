6064f555da56ad24195057a92030dc3d0d0391aa
scherbinek <scherbinek@users.noreply.github.com>
2023-08-29 18:25:12 +0200

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -4,4 +4,4 @@
 export $(grep -v '^#' ./resources/.env | xargs)
 echo $MESSAGE
     
-/usr/bin/rsync -azrd --delete -e 'ssh -p23 -o StrictHostKeyChecking=no' $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_DOCKER/ /dockerData/
+/usr/bin/rsync -azrd --delete -e 'ssh -p23 -o StrictHostKeyChecking=no' $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_DOCKER/ ./resources/dockerData
