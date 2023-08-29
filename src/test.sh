9ad306dacfd3a660f1e1610076b1fbc767c4c61a
scherbinek <scherbinek@users.noreply.github.com>
2023-08-29 13:49:16 +0200

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-# Check if we are running as sudo, if not, exit
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root. Maybe try 'sudo !!'"
-    exit
-fi
-
-if [ -z "$1" ]; then
-    echo "You need to specify arguments for this script."
-    echo "bash mainserver.sh secrets # Allow script to run, after you have configured secrets and SSH private key."
-    echo "bash mainserver.sh secrets sync # Sync data from backup server."
-    exit
-fi
-
-echo $1
+export $(grep -v '^#' ./resources/.env | xargs)
