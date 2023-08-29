a7cd88dccfefe819768595e7e1a598814e40ff16
scherbinek <scherbinek@users.noreply.github.com>
2023-08-29 17:19:58 +0200

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -4,4 +4,4 @@ export $(grep -v '^#' ./resources/.env | xargs)
 echo $MESSAGE
 sleep 10
 ## Home Folder
-/usr/bin/rsync -azrdu --delete -e 'ssh -p23 -o StrictHostKeyChecking=no' $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_ROOTHOMEFOLDER/ /root/
+/usr/bin/rsync -azrdu --delete -e 'ssh -p23' $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_ROOTHOMEFOLDER/ /root/ #-o StrictHostKeyChecking=no
