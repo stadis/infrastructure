879980486c441aef1e43a091add47aef55e08616
scherbinek <scherbinek@users.noreply.github.com>
2023-08-29 08:42:15 +0200

    Update orgserver-sync.sh

--- src/orgserver-sync.sh
+++ src/orgserver-sync.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+DestFolder_WWW="backups/ms/www"
+DestFolder_WWW_letsencrypt="backups/ms/www/etc-letsencrypt"
+DestFolder_Docker="backups/ms/docker"
+DestFolder_RootHomeFolder="backups/ms/root-home"
+DestSSHInfo="u364842@u364842.your-storagebox.de"
+
 export $(grep -v '^#' resources/.env | xargs)
 
 # Sync data from backup server
 DestSSHInfo="u364842@u364842.your-storagebox.de"
 # /usr/bin/rsync -azrdu --delete -e 'ssh -p23 -o StrictHostKeyChecking=no' u364842@u364842.your-storagebox.de:backups/ms/www/WWW-SQL-Dump.sql /tmp/sql-dump.sql
-/usr/bin/rsync -azrdu --delete -e 'ssh -p23 -o StrictHostKeyChecking=no' DestSSHInfo:backups/ms/www/WWW-SQL-Dump.sql /tmp/sql-dump.sql
+/usr/bin/rsync -azrdu --delete -e 'ssh -p23 -o StrictHostKeyChecking=no' $DestSSHInfo:$DestFolder_WWW/WWW-SQL-Dump.sql /tmp/sql-dump.sql
 /usr/bin/cat /tmp/sql-dump.sql | /usr/bin/docker exec -i bookstack_db /usr/bin/mysql -u root -p$BOOKSTACK__MYSQL_ROOT_PASSWORD
 /usr/bin/rm /tmp/sql-dump.sql
 ## Home Folder
-/usr/bin/rsync -azrdu --delete -e 'ssh -p23 -o StrictHostKeyChecking=no' DestSSHInfo:backups/ms/root-home/ /root/
+/usr/bin/rsync -azrdu --delete -e 'ssh -p23 -o StrictHostKeyChecking=no' $DestSSHInfo:$DestFolder_RootHomeFolder/ /root/
 ## Docker Stuff
 /usr/bin/mkdir -p /dockerData
-/usr/bin/rsync -azrdu --delete -e 'ssh -p23 -o StrictHostKeyChecking=no' DestSSHInfo:backups/hs/docker/ /dockerData/
+/usr/bin/rsync -azrdu --delete -e 'ssh -p23 -o StrictHostKeyChecking=no' $DestSSHInfo:$DestFolder_Docker/ /dockerData/
