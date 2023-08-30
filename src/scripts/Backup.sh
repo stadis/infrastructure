3bf050d92ef1425373df0118433a6c829a7b2818
scherbinek <scherbinek@users.noreply.github.com>
2023-08-30 13:01:20 +0200

    Update Backup.sh

--- src/scripts/Backup.sh
+++ src/scripts/Backup.sh
@@ -21,18 +21,18 @@ export $(grep -v '^#' ~stadisadm/infrastructure/src/resources/.env | xargs) # SU
 # Org Server
 ## MySQL
 /usr/bin/docker exec bookstack_db sh -c 'exec /usr/bin/mysqldump --all-databases --single-transaction --quick --lock-tables=false -u root -p$BOOKSTACK__MYSQL_ROOT_PASSWORD' > /tmp/sql-dump.sql
-/usr/bin/rsync -e 'ssh -p23 -i ~stadisadm/.ssh/id_rsa' -az /tmp/sql-dump.sql $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_WWW/WWW-SQL-Dump.sql
+/usr/bin/rsync -e 'ssh -p23 -i ~stadisadm/.ssh/id_rsa -o StrictHostKeyChecking=no' -az /tmp/sql-dump.sql $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_WWW/WWW-SQL-Dump.sql
 /usr/bin/rm /tmp/sql-dump.sql
 
 # Stop running Container
 /usr/bin/docker stop $(/usr/bin/docker ps -a -q)
 sleep 15
 ## /dockerData
-/usr/bin/rsync -e 'ssh -p23 -i ~stadisadm/.ssh/id_rsa' -azrd --delete ~stadisadm/infrastructure/src/resources/dockerData/* $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_DOCKER/
+/usr/bin/rsync -e 'ssh -p23 -i ~stadisadm/.ssh/id_rsa -o StrictHostKeyChecking=no' -azrd --delete ~stadisadm/infrastructure/src/resources/dockerData/* $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_DOCKER/
 sleep 5
 # Start all stopped Container
 /usr/bin/docker restart $(/usr/bin/docker ps -a -q)
 ## /etc/letsencrypt
-/usr/bin/rsync -e 'ssh -p23 -i ~stadisadm/.ssh/id_rsa' -azrd --delete /etc/letsencrypt/* $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_WWW_LETSENCRYPT/
+/usr/bin/rsync -e 'ssh -p23 -i ~stadisadm/.ssh/id_rsa -o StrictHostKeyChecking=no' -azrd --delete /etc/letsencrypt/* $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_WWW_LETSENCRYPT/
 ## /root
-/usr/bin/rsync -e 'ssh -p23 -i ~stadisadm/.ssh/id_rsa' -azrd --delete /root/* $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_ROOTHOMEFOLDER/
+/usr/bin/rsync -e 'ssh -p23 -i ~stadisadm/.ssh/id_rsa -o StrictHostKeyChecking=no' -azrd --delete /root/* $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_ROOTHOMEFOLDER/
