3e780a7f3e2807209c90f9f09311b80f52bac450
scherbinek <scherbinek@users.noreply.github.com>
2023-08-30 21:44:32 +0200

    Update appserver.sh

--- src/appserver.sh
+++ src/appserver.sh
@@ -52,7 +52,7 @@ software-properties-common
 /usr/bin/snap set certbot trust-plugin-with-root=ok
 
 # Let's Encrypt Certificates
-certbot certonly --nginx --agree-tos --preferred-challenges http -d tools.stadis.de,docs.stadis.de,vault.stadis.de,portainer-org.stadis.de -m marcel@scherbinek.de --redirect
+certbot certonly --nginx --agree-tos --preferred-challenges http -d tools.stadis.de,docs.stadis.de,vault.stadis.de,portainer-app.stadis.de -m marcel@scherbinek.de --redirect
 # Update and Restart Nginx Reverse Proxy
 /usr/bin/cp ./resources/default /etc/nginx/sites-available/default
 /usr/bin/systemctl restart nginx
