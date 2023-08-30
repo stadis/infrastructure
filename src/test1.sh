63fd358c43e112c127a9e937afb1bef06f302f82
scherbinek <scherbinek@users.noreply.github.com>
2023-08-30 12:57:23 +0200

    Update test1.sh

--- src/test1.sh
+++ src/test1.sh
@@ -4,8 +4,8 @@
 export $(grep -v '^#' ./resources/.env | xargs)
 echo $MESSAGE
 
-/usr/bin/rsync -azrd --delete -e 'ssh -p23' $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_ROOTHOMEFOLDER/ /root
+/usr/bin/rsync -azrd --delete -e 'ssh -p23 -i ~stadisadm/.ssh/id_rsa' $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_ROOTHOMEFOLDER/ /root
 ## Docker Stuff
 /usr/bin/mkdir -p ./resources/dockerData
 # /usr/bin/rsync -azrd --delete -e 'ssh -p23 -o StrictHostKeyChecking=no' u364842@u364842.your-storagebox.de:backups/ms/docker/ ./resources/dockerData
-/usr/bin/rsync -azrd --delete -e 'ssh -p23' $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_DOCKER/ ./resources/dockerData
+/usr/bin/rsync -azrd --delete -e 'ssh -p23 -i ~stadisadm/.ssh/id_rsa' $RSYNC__DESTSSHINFO:$RSYNC__DESTFOLDER_DOCKER/ ./resources/dockerData
