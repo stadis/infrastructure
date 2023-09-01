3ec8a1b562d0483d9b02ea408280f797780fd6bd
scherbinek <scherbinek@users.noreply.github.com>
2023-09-01 14:37:03 +0200

    Update appserver.sh

--- src/appserver.sh
+++ src/appserver.sh
@@ -45,6 +45,7 @@ software-properties-common
 # Uncomment cron logging (default: /etc/rsyslog.conf)
 /usr/bin/sed -i '/cron.*/s/^#//g' /etc/rsyslog.d/50-default.conf
 # Add crontabs or check by sudo crontab -u root -e
+(/usr/bin/crontab -l ; echo "0 0 * * * certbot renew --keep-until-expiring") | /usr/bin/crontab -
 (/usr/bin/crontab -l ; echo "0 1 * * * /bin/bash /root/Scripts/Backup.sh") | /usr/bin/crontab -
 (/usr/bin/crontab -l ; echo "0 2 * * * /usr/bin/docker image prune -a -f && /usr/bin/docker volume prune -f && /usr/bin/docker network prune -f") | /usr/bin/crontab -
 service rsyslog restart
